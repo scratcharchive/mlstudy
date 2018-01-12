9e321f0e69334a52ab1953c6af6798243217bc29
Jeremy Magland <jeremy.magland@gmail.com>
2018-01-12 17:18:51 -0500

    testing

--- mlstudyserver.js
+++ mlstudyserver.js
@@ -4,7 +4,7 @@ var app = express();
 app.set('port', (process.env.PORT || 5081));
 app.use(express.static(__dirname+'/web'));
 
-//app.listen(app.get('port'), function() {
-//	console.info('mlstudy is running on port:: '+app.get('port'), {port:app.get('port')});
-//});
+app.listen(app.get('port'), function() {
+	console.info('mlstudy is running on port:: '+app.get('port'), {port:app.get('port')});
+});
 
